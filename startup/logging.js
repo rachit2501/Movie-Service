2bafdce968b23a91d8d2f8d6a02612c6e1493edb
rachit2501 <rac.sri25@gmail.com>
2019-07-07 04:50:30 +0530

    logging errors

--- startup/logging.js
+++ startup/logging.js
@@ -13,5 +13,9 @@ module.exports = function()
   });
 
   winston.add(winston.transports.File , {filename : 'logfile.log'});
-  winston.add(winston.transports.MongoDB)
+  winston.add(winston.transports.MongoDB,
+  {
+    db: 'mongodb://localhost/vidly',
+    level : 'info'
+  });
 }

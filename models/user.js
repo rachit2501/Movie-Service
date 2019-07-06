98e8c72424584bd0a4908ab6e0d6353f7fcd0e92
rachit2501 <rac.sri25@gmail.com>
2019-07-07 04:02:10 +0530

    Completed app testing left

---
+++ models/user.js
@@ -0,0 +1,45 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const config = require('config');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type : String,
+		required : true,
+		minlength : 5,
+		maxlength: 50
+	},
+	email:
+	{
+		type: String,
+		required : true,
+		minlength : 5,
+		maxlength : 255,
+		unique : true
+	},
+
+	password:
+	{
+		type: String,
+		required : true,
+		minlength : 5,
+		maxlength : 1024
+	}
+});
+
+const User = mongoose.model('User', userSchema);
+
+function validateUser ( User)
+{	
+	const schema = {
+		name: Joi.string().min(5).max(50).required(),
+		email : Joi.string().min(5).max(255).required().email(),
+		password: Joi.string().min(5).max(255).required()
+	};
+	
+	return Joi.validate(User,schema);
+}
+
+exports.User = User;
+exports.validate = validateUser;
---
+++ startup/logging.js
@@ -0,0 +1,17 @@
+const winston = require('winston');
+
+require('winstein-mongodb');
+require('express-async-errors');
+
+module.exports = function()
+{
+  winston.handleException(
+    new winston.transports.File({ filename :  'uncaughtException.log'}));
+  process.on('unhandledRejection', (ex)=>
+  {
+    throw ex;
+  });
+
+  winston.add(winston.transports.File , {filename : 'logfile.log'});
+  winston.add(winston.transports.MongoDB)
+}

008c01947cb31443c1f4d28f704669193596eb54
rachit2501 <rac.sri25@gmail.com>
2019-07-10 21:46:14 +0530

    integration test

--- middleware/async.js
+++ middleware/async.js
@@ -1,13 +1,10 @@
-module.exports = function(handler)
-{
-	return async (req , res  , next)=>
-	{
-		try{
-			await handler(req,res);
-		}
-		catch(ex)
-		{
-			next(ex);
-		}
-	};
+module.exports = function (handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    }
+    catch(ex) {
+      next(ex);
+    }
+  };  
 }
--- startup/logging.js
+++ startup/logging.js
@@ -1,21 +1,19 @@
 const winston = require('winston');
-
-require('winstein-mongodb');
+// require('winston-mongodb');
 require('express-async-errors');
 
-module.exports = function()
-{
-  winston.handleException(
-    new winston.transports.File({ filename :  'uncaughtException.log'}));
-  process.on('unhandledRejection', (ex)=>
-  {
+module.exports = function() {
+  winston.handleExceptions(
+    new winston.transports.Console({ colorize: true, prettyPrint: true }),
+    new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
+  
+  process.on('unhandledRejection', (ex) => {
     throw ex;
   });
-
-  winston.add(winston.transports.File , {filename : 'logfile.log'});
-  winston.add(winston.transports.MongoDB,
-  {
-    db: 'mongodb://localhost/vidly',
-    level : 'info'
-  });
+  
+  winston.add(winston.transports.File, { filename: 'logfile.log' });
+  // winston.add(winston.transports.MongoDB, { 
+  //   db: 'mongodb://localhost/vidly',
+  //   level: 'info'
+  // });  
 }
